a3bdebbcf3473ba6b6d91a3b3bc2790126d74eab
chris <1831903054@qq.com>
2022-09-10 20:27:42 +0800

    koa项目架构

---
+++ note.md
@@ -0,0 +1,20 @@
+<!-- 技术选型 -->
+
+# 框架选型  koa2 vs express vs egg
+express 基于js回调
+egg 
+
+# 数据库选型 《mysql vs mongodb》
+    mysql 
+
+# 登录技术 （session vs jwt）
+    session  jwt 适合前后端分离的项目
+
+# 前端页面 （ejs 后端模板引擎 vs vue/react 前端框架）
+    ejs 
+
+# 缓存数据库 （redis ）
+
+# 单元测试（ jest ）
+
+# 代码和结构
---
+++ src/app.js
@@ -0,0 +1,28 @@
+const Koa = require('koa')
+const app = new Koa()
+const views = require('koa-views')
+const json = require('koa-json')
+const onerror = require('koa-onerror')
+const bodyparser = require('koa-bodyparser')
+const logger = require('koa-logger')
+
+// error hanlder
+onerror(app)
+
+// middlewares
+app.use(bodyparser({
+    enableTypes:['json', 'form', 'text']
+}))
+app.use(json())
+app.use(logger())
+app.use(require('koa-static')(__dirname+'/public'))
+app.use(views(__dirname+'/views',{
+    extension: 'ejs'
+}))
+
+// error-handling
+app.on('error', (err, ctx) => {
+    console.error('server error', err, ctx)
+})
+
+module.exports = app
